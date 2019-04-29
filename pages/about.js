ae98f9e5388869b1e46260d7604ba87fd7167d19
evanszymkowicz <evan.szymkowicz@evanwolf.co>
2019-04-28 21:29:29 -0400

    .next

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const About = () => (
+	<Layout>
+    <div>
+      <h1>Welcome to BitzPrice</h1>
+      <p>Check current Bitcoin rate</p>
+    </div>
+  </Layout>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+
+const Index = () => (
+	<Layout>
+    <div>
+      <h1>Welcome to BitzPrice</h1>
+      <p>Check current Bitcoin rate</p>
+    </div>
+  </Layout>
+);
+
+export default Index
