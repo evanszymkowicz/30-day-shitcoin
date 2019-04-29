a669c2360ab2f4f8af1752a8967eeaf96d60aead
evanszymkowicz <evan.szymkowicz@evanwolf.co>
2019-04-29 16:12:39 -0400

    tooling around with React for prices

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 30-day-shitcoin
 Bitcoin indexes are the new ToDo lists
+
+Using isomorphic-unfetch package to use Fetch API on client and server
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
+import Fetch from 'isomorphic-unfetch';
 import Navbar from '../components/Layout';
+import Prices from '../components/Prices';
 
 const Index = () => (
   <Layout>
     <div>
       <Navbar/>
   	  <h1>Welcome!</h1>
+      <p>Check current rate:</p>
+      <Prices bpi={props.bpi}/>
+      {props.time.updated} //this is from the API docs
     </div>
   </Layout>
 );
+
+Index.getinitialProps = asyn function() {
+  //here comes the fetch
+  const res = await fetch
+  ('https://api.coindesk/com/v1/bpi/currentprice.json');
+  //second await to map the result to JSON
+  const data = await res.json();
+
+  return {
+    bpi: data
+  };
+}
 
 export default Index;
